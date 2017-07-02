dc045251d7d7b5a85e774fb4ef2a49c4b805a3f6
zkochan <zoltan.kochan@gmail.com>
2017-07-03 01:17:26 +0300

    feat: init

---
+++ README.md
@@ -0,0 +1,37 @@
+# rename-overwrite
+
+[![Status](https://travis-ci.org/zkochan/rename-overwrite.svg?branch=master)](https://travis-ci.org/zkochan/rename-overwrite "See test builds")
+
+> Like `fs.rename` but overwrites existing file or directory
+
+## Install
+
+Install it via npm.
+
+```
+npm install rename-overwrite
+```
+
+## Usage
+
+```js
+const renameOverwrite = require('rename-overwrite')
+
+renameOverwrite('old-dir-name', 'new-dir-name')
+  .then(() => console.log('done'))
+  .catch(err => console.log(err))
+```
+
+## API
+
+### `renameOverwrite(oldPath, newPath) => Promise<void>`
+
+Renames a file or directory asynchronously. Overwrites existing file or directory.
+
+### `renameOverwrite.sync(oldPath, newPath)`
+
+Renames a file or directory synchronously. Overwrites existing file or directory.
+
+## License
+
+[MIT](LICENSE)
---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict'
+const fs = require('graceful-fs')
+const thenify = require('thenify')
+const rimraf = thenify(require('rimraf'))
+const rimrafSync = require('rimraf').sync
+const timeout = require('timeout-then')
+
+const rename = thenify(fs.rename)
+
+module.exports = function renameOverwrite (oldPath, newPath) {
+  return rename(oldPath, newPath)
+    .catch(err => {
+      switch (err.code) {
+        case 'ENOTEMPTY':
+        case 'EEXIST':
+          return rimraf(newPath)
+            .then(() => rename(oldPath, newPath))
+        // weird Windows shit
+        case 'EPERM':
+          return timeout(200)
+            .then(() => rimraf(newPath))
+            .then(() => rename(oldPath, newPath))
+        default:
+          throw err
+      }
+    })
+}
+
+module.exports.sync = function renameOverwriteSync (oldPath, newPath) {
+  try {
+    fs.renameSync(oldPath, newPath)
+  } catch (err) {
+    switch (err.code) {
+      case 'ENOTEMPTY':
+      case 'EEXIST':
+      case 'EPERM': // weird Windows shit
+        rimrafSync(newPath)
+        fs.renameSync(oldPath, newPath)
+        return
+      default:
+        throw err
+    }
+  }
+}
