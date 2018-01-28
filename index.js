a901a124851589cd394f8dca7bdfb4300262e438
Zoltan Kochan <z@kochan.io>
2018-01-28 19:12:53 +0200

    fix: retry more on EPERM
    ref pnpm/pnpm#1015

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ const fs = require('graceful-fs')
 const promisify = require('util.promisify')
 const rimraf = promisify(require('rimraf'))
 const rimrafSync = require('rimraf').sync
-const timeout = require('timeout-then')
+const delay = require('delay')
 
 const rename = promisify(fs.rename)
 
 module.exports = function renameOverwrite (oldPath, newPath) {
+  return _renameOverwrite(oldPath, newPath, 0, 200)
+}
+
+const MAX_RETRY = 8
+
+function _renameOverwrite (oldPath, newPath, tries, delayOnEperm) {
   return rename(oldPath, newPath)
     .catch(err => {
       switch (err.code) {
@@ -17,12 +23,22 @@ module.exports = function renameOverwrite (oldPath, newPath) {
             .then(() => rename(oldPath, newPath))
         // weird Windows stuff
         case 'EPERM':
-          return timeout(200)
+          return delay(200)
             .then(() => rimraf(newPath))
             .then(() => rename(oldPath, newPath))
         default:
           throw err
       }
+    })
+    .catch(err => {
+      // Windows Defender holds locks on newly created files
+      // so EPERM errors are retried a lot
+      // see related issue: https://github.com/pnpm/pnpm/issues/1015
+      if (err.code !== 'EPERM' || tries >= MAX_RETRY) {
+        throw err
+      }
+      return delay(delayOnEperm)
+        .then(() => _renameOverwrite(oldPath, newPath, tries + 1, delayOnEperm + 200))
     })
 }
 
